814d49670657670bd0bab08c9bb560f1896fe0d7
Xujie Song (Surface) <133342301@163.com>
2017-12-04 08:16:30 +1100

    Updates

--- noodlezero-australia-fastway-au-shipping-zone.php
+++ noodlezero-australia-fastway-au-shipping-zone.php
@@ -555,7 +555,7 @@ function fastway_au_shipping_zone_method() {
 								$rate = array(
 									'id' => $this->id . "-parcel",
 									'label' => $this->title . " - Parcel (" . $result->result->delivery_timeframe_days . " Days) ",
-									'cost' => $parcel_price,
+									'cost' => $r->totalprice_frequent,
 									'taxes' => false,
 								);
 
